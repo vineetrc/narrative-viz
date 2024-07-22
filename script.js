a5a89e2b5f61c535915422c06ad2105fd96136fb
Vineet Chinthakindi <vchintha@Vineets-New-MacBook-Pro.local>
2024-07-22 12:04:52 -0700

    first chart

--- script.js
+++ script.js
@@ -1,112 +1,70 @@
 document.addEventListener('DOMContentLoaded', function() {
-    d3.csv('./data/cleaned_nba_playoff_stats.csv', function(d) {
-        return {
-            player: d.Player,
-            pts: +d.PTS,
-            fg_perc: +d['FG%'],
-            threep_perc: +d['3P%'],
-            trb: +d.TRB,
-            ast: +d.AST,
-            stl: +d.STL
-        };
-    }).then(function(data) {
-        // Scene 1: Overview of Player Statistics
-        const topPlayers = data.sort((a, b) => b.pts - a.pts).slice(0, 10);
-        
+    d3.csv('./data/cleaned_nba_playoff_stats.csv').then(function(data) {
+        data.forEach(function(d) {
+            d.pts = +d.PTS;
+            d.fg_perc = +d['FG%'];
+            d.threep_perc = +d['3P%'];
+            d.twop_perc = +d['2P%'];
+            d.trb = +d.TRB;
+            d.ast = +d.AST;
+            d.stl = +d.STL;
+        });
+
+        // Scene 1: Scatterplot of 3P% vs 2P% with circle size representing average points and color representing team
         const svg1 = d3.select('#chart1')
             .append('svg')
             .attr('width', 800)
-            .attr('height', 400);
-        
-        const xScale1 = d3.scaleBand()
-            .domain(topPlayers.map(d => d.player))
-            .range([0, 800])
-            .padding(0.1);
+            .attr('height', 600);
 
-        const yScale1 = d3.scaleLinear()
-            .domain([0, d3.max(topPlayers, d => d.pts)])
-            .range([400, 0]);
+        const xScale = d3.scaleLinear()
+            .domain([0, d3.max(data, d => d.threep_perc)])
+            .range([50, 750]);
 
-        svg1.selectAll('rect')
-            .data(topPlayers)
-            .enter()
-            .append('rect')
-            .attr('x', d => xScale1(d.player))
-            .attr('y', d => yScale1(d.pts))
-            .attr('width', xScale1.bandwidth())
-            .attr('height', d => 400 - yScale1(d.pts))
-            .attr('fill', 'steelblue');
+        const yScale = d3.scaleLinear()
+            .domain([0, d3.max(data, d => d.twop_perc)])
+            .range([550, 50]);
 
-        svg1.append('g')
-            .attr('transform', 'translate(0, 400)')
-            .call(d3.axisBottom(xScale1));
+        const rScale = d3.scaleSqrt()
+            .domain([0, d3.max(data, d => d.pts)])
+            .range([5, 20]);
 
-        svg1.append('g')
-            .call(d3.axisLeft(yScale1));
+        const colorScale = d3.scaleOrdinal(d3.schemeCategory10);
 
-        // Scene 2: Shooting Efficiency
-        const svg2 = d3.select('#chart2')
-            .append('svg')
-            .attr('width', 800)
-            .attr('height', 400);
-
-        const xScale2 = d3.scaleLinear()
-            .domain([0, 100])
-            .range([0, 800]);
-
-        const yScale2 = d3.scaleLinear()
-            .domain([0, 100])
-            .range([400, 0]);
-
-        svg2.selectAll('circle')
+        svg1.selectAll('circle')
             .data(data)
             .enter()
             .append('circle')
-            .attr('cx', d => xScale2(d.fg_perc))
-            .attr('cy', d => yScale2(d.threep_perc))
-            .attr('r', 5)
-            .attr('fill', 'orange');
+            .attr('cx', d => xScale(d.threep_perc))
+            .attr('cy', d => yScale(d.twop_perc))
+            .attr('r', d => rScale(d.pts))
+            .attr('fill', d => colorScale(d.Tm))
+            .attr('stroke', 'black')
+            .attr('stroke-width', 1)
+            .append('title')
+            .text(d => `${d.Player}: ${d.pts} points`);
 
-        svg2.append('g')
-            .attr('transform', 'translate(0, 400)')
-            .call(d3.axisBottom(xScale2));
+        svg1.append('g')
+            .attr('transform', 'translate(0, 550)')
+            .call(d3.axisBottom(xScale).ticks(10).tickFormat(d => d + "%"));
 
-        svg2.append('g')
-            .call(d3.axisLeft(yScale2));
+        svg1.append('g')
+            .attr('transform', 'translate(50, 0)')
+            .call(d3.axisLeft(yScale).ticks(10).tickFormat(d => d + "%"));
 
-        // Scene 3: Player Contributions
-        const topContributors = data.sort((a, b) => (b.trb + b.ast + b.stl) - (a.trb + a.ast + a.stl)).slice(0, 10);
+        svg1.append('text')
+            .attr('x', 400)
+            .attr('y', 590)
+            .attr('text-anchor', 'middle')
+            .attr('font-size', '12px')
+            .text('3-Point Percentage');
 
-        const svg3 = d3.select('#chart3')
-            .append('svg')
-            .attr('width', 800)
-            .attr('height', 400);
-
-        const xScale3 = d3.scaleBand()
-            .domain(topContributors.map(d => d.player))
-            .range([0, 800])
-            .padding(0.1);
-
-        const yScale3 = d3.scaleLinear()
-            .domain([0, d3.max(topContributors, d => d.trb + d.ast + d.stl)])
-            .range([400, 0]);
-
-        svg3.selectAll('rect')
-            .data(topContributors)
-            .enter()
-            .append('rect')
-            .attr('x', d => xScale3(d.player))
-            .attr('y', d => yScale3(d.trb + d.ast + d.stl))
-            .attr('width', xScale3.bandwidth())
-            .attr('height', d => 400 - yScale3(d.trb + d.ast + d.stl))
-            .attr('fill', 'green');
-
-        svg3.append('g')
-            .attr('transform', 'translate(0, 400)')
-            .call(d3.axisBottom(xScale3));
-
-        svg3.append('g')
-            .call(d3.axisLeft(yScale3));
+        svg1.append('text')
+            .attr('x', -300)
+            .attr('y', 20)
+            .attr('text-anchor', 'middle')
+            .attr('font-size', '12px')
+            .attr('transform', 'rotate(-90)')
+            .text('2-Point Percentage');
     });
 });
 
