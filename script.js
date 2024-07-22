c671d180983d345424eefdcdf86da80d8a9cf93c
Vineet Chinthakindi <vchintha@Vineets-New-MacBook-Pro.local>
2024-07-22 10:58:13 -0700

    added nba data

--- script.js
+++ script.js
@@ -1,63 +1,113 @@
 document.addEventListener('DOMContentLoaded', function() {
-    // Data for the visualization
-    const data = [10, 15, 20, 25, 30];
-    
-    // Scene 1: Introduction
-    const svg1 = d3.select('#chart1')
-        .append('svg')
-        .attr('width', 500)
-        .attr('height', 300);
-    
-    svg1.selectAll('circle')
-        .data(data)
-        .enter()
-        .append('circle')
-        .attr('cx', (d, i) => (i + 1) * 80)
-        .attr('cy', 150)
-        .attr('r', d => d)
-        .attr('fill', 'steelblue');
+    d3.csv('./data/cleaned_nba_playoff_stats.csv', function(d) {
+        return {
+            player: d.Player,
+            pts: +d.PTS,
+            fg_perc: +d['FG%'],
+            threep_perc: +d['3P%'],
+            trb: +d.TRB,
+            ast: +d.AST,
+            stl: +d.STL
+        };
+    }).then(function(data) {
+        // Scene 1: Overview of Player Statistics
+        const topPlayers = data.sort((a, b) => b.pts - a.pts).slice(0, 10);
+        
+        const svg1 = d3.select('#chart1')
+            .append('svg')
+            .attr('width', 800)
+            .attr('height', 400);
+        
+        const xScale1 = d3.scaleBand()
+            .domain(topPlayers.map(d => d.player))
+            .range([0, 800])
+            .padding(0.1);
 
-    // Scene 2: Details
-    const svg2 = d3.select('#chart2')
-        .append('svg')
-        .attr('width', 500)
-        .attr('height', 300);
+        const yScale1 = d3.scaleLinear()
+            .domain([0, d3.max(topPlayers, d => d.pts)])
+            .range([400, 0]);
 
-    svg2.selectAll('rect')
-        .data(data)
-        .enter()
-        .append('rect')
-        .attr('x', (d, i) => i * 100)
-        .attr('y', d => 300 - d * 10)
-        .attr('width', 80)
-        .attr('height', d => d * 10)
-        .attr('fill', 'orange');
+        svg1.selectAll('rect')
+            .data(topPlayers)
+            .enter()
+            .append('rect')
+            .attr('x', d => xScale1(d.player))
+            .attr('y', d => yScale1(d.pts))
+            .attr('width', xScale1.bandwidth())
+            .attr('height', d => 400 - yScale1(d.pts))
+            .attr('fill', 'steelblue');
 
-    // Scene 3: Conclusion
-    const svg3 = d3.select('#chart3')
-        .append('svg')
-        .attr('width', 500)
-        .attr('height', 300);
+        svg1.append('g')
+            .attr('transform', 'translate(0, 400)')
+            .call(d3.axisBottom(xScale1));
 
-    svg3.selectAll('line')
-        .data(data)
-        .enter()
-        .append('line')
-        .attr('x1', (d, i) => i * 100)
-        .attr('y1', 0)
-        .attr('x2', (d, i) => i * 100)
-        .attr('y2', d => d * 10)
-        .attr('stroke', 'green')
-        .attr('stroke-width', 2);
+        svg1.append('g')
+            .call(d3.axisLeft(yScale1));
 
-    // Add interactivity (e.g., tooltips)
-    svg1.selectAll('circle')
-        .on('mouseover', function(event, d) {
-            d3.select(this).attr('fill', 'red');
-        })
-        .on('mouseout', function(event, d) {
-            d3.select(this).attr('fill', 'steelblue');
-        });
+        // Scene 2: Shooting Efficiency
+        const svg2 = d3.select('#chart2')
+            .append('svg')
+            .attr('width', 800)
+            .attr('height', 400);
+
+        const xScale2 = d3.scaleLinear()
+            .domain([0, 100])
+            .range([0, 800]);
+
+        const yScale2 = d3.scaleLinear()
+            .domain([0, 100])
+            .range([400, 0]);
+
+        svg2.selectAll('circle')
+            .data(data)
+            .enter()
+            .append('circle')
+            .attr('cx', d => xScale2(d.fg_perc))
+            .attr('cy', d => yScale2(d.threep_perc))
+            .attr('r', 5)
+            .attr('fill', 'orange');
+
+        svg2.append('g')
+            .attr('transform', 'translate(0, 400)')
+            .call(d3.axisBottom(xScale2));
+
+        svg2.append('g')
+            .call(d3.axisLeft(yScale2));
+
+        // Scene 3: Player Contributions
+        const topContributors = data.sort((a, b) => (b.trb + b.ast + b.stl) - (a.trb + a.ast + a.stl)).slice(0, 10);
+
+        const svg3 = d3.select('#chart3')
+            .append('svg')
+            .attr('width', 800)
+            .attr('height', 400);
+
+        const xScale3 = d3.scaleBand()
+            .domain(topContributors.map(d => d.player))
+            .range([0, 800])
+            .padding(0.1);
+
+        const yScale3 = d3.scaleLinear()
+            .domain([0, d3.max(topContributors, d => d.trb + d.ast + d.stl)])
+            .range([400, 0]);
+
+        svg3.selectAll('rect')
+            .data(topContributors)
+            .enter()
+            .append('rect')
+            .attr('x', d => xScale3(d.player))
+            .attr('y', d => yScale3(d.trb + d.ast + d.stl))
+            .attr('width', xScale3.bandwidth())
+            .attr('height', d => 400 - yScale3(d.trb + d.ast + d.stl))
+            .attr('fill', 'green');
+
+        svg3.append('g')
+            .attr('transform', 'translate(0, 400)')
+            .call(d3.axisBottom(xScale3));
+
+        svg3.append('g')
+            .call(d3.axisLeft(yScale3));
+    });
 });
 
 let currentScene = 1;
